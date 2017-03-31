ca83f1d04d809c9f6df20742b1ba85f36bf4b2b0
tomn2008 <tomn2008@gmail.com>
2017-03-31 19:22:55 +0200

    1) Adding files for second part of assignment

---
+++ js/data_objects.js
@@ -0,0 +1,33 @@
+var entireData =  {
+	siteName: "Modern Developer",
+	books:[{
+				title: "Eloquent JavaScript: </br>A Modern Introduction to Programming",
+				author: "Marijn Haverbeke",
+				published: "Published on 2016",
+				edition: "1st Edition",
+				description :  '<p style="">JavaScript lies at the heart of almost every modern web application. From social apps to'+
+      							'the newest browser based games. Though simple for beginners to pick up and play with, JavaScript is a flexible complex language that you can use to build full scale applications.'+
+                            	'</p><p style="">Eloquent JavaScript, 2nd edition dives deep into the JavaScript language to write beautiful, effective code.</p><a href="#" id="read_more"class="blue_menu_text">Read more</a>',
+				coverImage:  "images/eloquent_javascript.png"
+			},
+			{
+				title: 'Javascript and JQuery: Interactive Front-End </br>Web Development',
+				author: "Marijn Haverbeke",
+				published: "Published on 2016",
+				edition: "1st Edition",
+				description :  '<p>In JavaScript & jQuery renowned author Jon Duckett dis-cards the traditional programming book template and approaches writing code in a more relevant, less intimidating way.</p><p>Full-color and packed with instructional graphics and photos, his books have gained a loyal following by illustrating programming in a way both instructive for newcomers and invaluable for seasoned coders.</p><a href="#" id="read_more"class="blue_menu_text">Read more</a>',
+				coverImage:  "images/javascript_jq.png"
+			},
+			{
+				title: "Learning PHP, MySQl & Javascript: With jQuery,</br>CSS & HTML5",
+				author: "Marijn Haverbeke",
+				published: "Published on 2016",
+				edition: "4th Edition",
+				description : "<p >Build interactive, data-driven websites with the potent combination of open-source technologies and web standards, even if you have only basic HTML knowledge. With" +
+							"this popular hands-on guide, you'll tackle dynamic web programming with the help of today's core technologies: PHP, MySQL, JavaScript, jQuery, CSS and HTML5." +
+                            "</p><p >Eloquent JavaScript, 2nd edition dives deep into the JavaScript language to write beautiful, effective code.</p>",
+				coverImage: "images/lrg.jpg"
+			}]
+		
+
+};
